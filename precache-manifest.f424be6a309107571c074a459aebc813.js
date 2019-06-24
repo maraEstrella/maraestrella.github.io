4176005fb9dd0de36c210e97c11e6faf6ca3f616
marali5 <mara_lilian@hotmail.com>
2019-06-24 13:17:53 -0500

    Updates

---
+++ precache-manifest.f424be6a309107571c074a459aebc813.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c3d46d9f61bdf7cacc9e4bd376e189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fece7403f298fb08093d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "da6514f96ded57507ef8",
+    "url": "/static/css/main.b52c288f.chunk.css"
+  },
+  {
+    "revision": "fece7403f298fb08093d",
+    "url": "/static/js/2.e7b966ef.chunk.js"
+  },
+  {
+    "revision": "da6514f96ded57507ef8",
+    "url": "/static/js/main.ca86081f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f92cd4189b4454fce08f4ba420d3131",
+    "url": "/static/media/Group.9f92cd41.png"
+  },
+  {
+    "revision": "8dd614e3f35363ad343f80dca03277b5",
+    "url": "/static/media/Mara.8dd614e3.jpg"
+  },
+  {
+    "revision": "71594ec5b2fa8fe614296bc702c29fa3",
+    "url": "/static/media/burger.71594ec5.png"
+  },
+  {
+    "revision": "c3c3657bdc122448e31db88b4d7a2b95",
+    "url": "/static/media/doorIn.c3c3657b.png"
+  },
+  {
+    "revision": "1e9069d1f797c66ba9b4073862a277b3",
+    "url": "/static/media/pokemon.1e9069d1.png"
+  },
+  {
+    "revision": "fcf65e5084a7a8904fc34f4086e03926",
+    "url": "/static/media/words.fcf65e50.png"
+  }
+]);
